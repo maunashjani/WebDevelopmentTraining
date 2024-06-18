0c67afa820e847ce43276cd55f5435e03397f904
Maunash Jani <maunash@live.com>
2024-06-18 16:05:45 +0530

    new fn

---
+++ js/index7.js
@@ -0,0 +1,33 @@
+let student = {
+    firstName: "John",
+    lastName: "Doe",
+    marks: 70,
+    email: "john@example.com",
+    address: {
+        buildingName: "demo building",
+        city: "demo city",
+        pincode: 123456
+    },
+    displayMarks: function() {
+        return this.marks;
+    },
+    displayPincode: () => {
+        return this.address.pincode;
+    }
+};
+
+var output = document.getElementById("output");
+
+// output.innerHTML =
+//     `${student.firstName},
+// ${student.lastName},
+// ${student.marks},
+// ${student.email},
+// ${student.address.buildingName},
+// ${student.address.city},
+// ${student.address.pincode}
+// `;
+
+//output.innerHTML = student.displayMarks();
+
+output.innerHTML = student.displayPincode();
