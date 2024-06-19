0142e5c80b4ccdbe462291a1f7aae1e689012bda
Maunash Jani <maunash@live.com>
2024-06-19 13:03:44 +0530

    response headers

--- node.js/index1.js
+++ node.js/index1.js
@@ -1,6 +1,8 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World');
 });
 
