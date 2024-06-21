b5cab072e3327bb44fc3ccbb1cf002ae9d93e9bf
Maunash Jani <maunash@live.com>
2024-06-21 10:25:27 +0530

    pwa

--- pwa/app.js
+++ pwa/app.js
@@ -2,6 +2,20 @@ if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
         navigator.serviceWorker.register('/service-worker.js').then(registration => {
             console.log('ServiceWorker registration successful with scope: ', registration.scope);
+            
+            registration.onupdatefound = () => {
+                const installingWorker = registration.installing;
+                installingWorker.onstatechange = () => {
+                    if (installingWorker.state === 'installed') {
+                        if (navigator.serviceWorker.controller) {
+                            // New update available
+                            if (confirm("New update available. Reload to update?")) {
+                                window.location.reload();
+                            }
+                        }
+                    }
+                };
+            };
         }, err => {
             console.log('ServiceWorker registration failed: ', err);
         });
