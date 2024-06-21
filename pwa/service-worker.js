86d3b85a6abc8dbcdf87940de345e08e26816e9a
Maunash Jani <maunash@live.com>
2024-06-21 09:19:32 +0530

    pwa

---
+++ pwa/service-worker.js
@@ -0,0 +1,22 @@
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open('v1').then(cache => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/app.js',
+                '/manifest.json',
+                '/icons/icon1.png',
+                '/icons/icon2.png'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
