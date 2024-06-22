8dd7a7987d0bb7be989ee9976d030f921746586f
Maunash Jani <maunash@live.com>
2024-06-22 11:46:06 +0530

    CRUD

--- react/thirdproject/src/App.js
+++ react/thirdproject/src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from "react";
+import CRUDButtons from "./CRUDButtons";
+import CRUDTable from "./CRUDTable";
+import CRUDForm from "./CRUDForm";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  
+  return();
 }
 
 export default App;
